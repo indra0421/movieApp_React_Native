0defd39c96be65e04897072b6528b4124a8dfb18
Indranil Naskar <82460475+indra0421@users.noreply.github.com>
2024-06-05 06:58:31 +0530

    Update TopRated.js

--- src/movies/TopRated.js
+++ src/movies/TopRated.js
@@ -6,7 +6,7 @@ import MovieCard from '../cards/MovieCard'
 
 const TopRated = () => {
     const [lists, setLists] = useState();
-    const API_KEY2 = "30f76e3ddb7adb8e8c0dfc85fb5578e9"
+    const API_KEY2 = ""
     const apiUrl = `https://api.themoviedb.org/3/tv/top_rated?api_key=${API_KEY2}`
 
     useEffect(() => {
